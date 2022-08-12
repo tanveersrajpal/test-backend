242cf9a277c97ab3a4b800b4f10ae1b6e5c4a469
tanveersrajpal <tanveersrajpal@gmail.com>
2022-08-12 14:26:55 +1200

    first commit

--- Backend/TodoList.Api/TodoList.Api/obj/Release/net5.0/TodoList.Api.AssemblyInfo.cs
+++ Backend/TodoList.Api/TodoList.Api/obj/Release/net5.0/TodoList.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("30df8384-b270-41ca-82d4-e0859fe2ed08")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList.Api")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList.Api")]
