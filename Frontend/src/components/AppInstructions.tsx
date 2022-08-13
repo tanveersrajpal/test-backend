fd056dc1a4dba60be15c937d09757a999777ee36
Tanveer Rajpal <56073437+tanveersrajpal@users.noreply.github.com>
2022-08-13 22:45:10 +1200

    Update AppInstructions.tsx

--- Frontend/src/components/AppInstructions.tsx
+++ Frontend/src/components/AppInstructions.tsx
@@ -5,7 +5,7 @@ export const AppInstructions = () => (
     <Col>
       <Alert variant="success">
         <Alert.Heading>Todo List App</Alert.Heading>
-        Welcome to the ClearPoint SRE technical test.
+        Welcome to the ClearPoint SRE technical test - This has been Modified.
         <br />
         <br />
 
